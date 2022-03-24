e4ed9f04dc7a727d87a0ad212709035b9e1f05f5
Denise Pereira <deniseluvoir@gmail.com>
2022-03-24 13:29:41 -0300

    Retornos de função

--- kotlin/kotlin/src/main/kotlin/pratica/classe.kt
+++ kotlin/kotlin/src/main/kotlin/pratica/classe.kt
@@ -1,21 +1,14 @@
 package pratica
 
-// se a nota for >= 6 : Parabés você passou!
-// se a nota for > 4 : Você está de recuperação!
-// se a nota for <= 4: Você reprovou
-
-fun resultado (nota:Int){
-    if (nota >= 6){
-        println("Parabés você passou!")
-    }else if (nota >= 4){
-        println("Você está de recuperação!")
-    }else{
-        println("Você reprovou")
+//Mini mercadado
+fun carteira (valor:Int): String{
+    if(valor == 20){
+        return "Dois quilos de laranja"
+    }else if (valor == 30){
+        return "Dois pacotes de arroz e um quilo de carne"
     }
+    return "Saldo insuficiente"
 }
-
 fun main (){
-    resultado(6)
-    resultado(4)
-    resultado(1)
+    println(carteira(3))
 }
